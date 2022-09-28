dd5267c89d9c2755acae7aaa156656ccbe9525a9
AnonimniyGeniy <68807643+AnonimniyGeniy@users.noreply.github.com>
2022-09-28 15:43:13 +0300

    Constructor fix commit

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,40 +1,54 @@
 
 public class Lab1 {
-    long a[];
-    double x[];
-    double a1[][];
-    public void Print(){
-        for(int i = 0; i < 16; i++){
-            for(int j = 0; j < 17; j++){
+    static long a[];
+    static double x[];
+    static double a1[][];
+    /*
+    public void Print() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 17; j++) {
                 System.out.printf("%.2f; ", a1[i][j]);
                 //System.out.println(a1[i][j]);
             }
             System.out.print("\n");
         }
     }
-    public Lab1(){
+    */
+    public static void Print() {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 17; j++) {
+                System.out.printf("%.2f; ", a1[i][j]);
+                //System.out.println(a1[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
+    public static void Lab1() {
+
         a = new long[16];
         x = new double[17];
         a1 = new double[16][17];
         boolean debug = false;
-        for(int i = 16; i > 0; i--){
+        for (int i = 16; i > 0; i--) {
             a[16 - i] = i;
         }
-        for(int i = 0; i < 17; i++){
+        for (int i = 0; i < 17; i++) {
             x[i] = ((Math.random() * 9) - 3);
         }
         double pt1 = 0;
         double pt2 = 0;
         double pt3 = 0;
-        for(int i = 0; i < 16; i++){
-            for(int j = 0; j < 17; j++) {
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 17; j++) {
                 switch ((int) a[i]) {
                     case 13:
                         pt1 = Math.pow(3 / (Math.pow(0.5 / x[j], 3)), (Math.pow(4 / (x[j] - 1), 2))); //first part of expression
                         pt2 = Math.pow(Math.pow((1.0 / 3) / x[j], 2), ((1.0 - Math.pow(x[j] - 1, 3)) / 4)); //2nd part of expression
                         pt3 = (Math.pow(((Math.atan((x[j] + 1.5) / 9) + 1) / (Math.pow(Math.E, x[j]))), 3) + 1.0 / 3); //3rd part of expression
                         a1[i][j] = Math.pow(pt1, pt2 * pt3);
-                        if (debug){System.out.printf("%.2f; ", a1[i][j]);}
+                        if (debug) {
+                            System.out.printf("%.2f; ", a1[i][j]);
+                        }
                         break;
                         /*
                         we should break in every case of switch to stop
@@ -50,21 +64,29 @@ public class Lab1 {
                     case 16:
                         pt1 = Math.pow(((1.0 / 4) / (Math.pow((3.0 / 4) / (1.0 / x[j]), 3) - 2 - 0.5)), Math.asin((x[j] + 1.5) / 9));
                         a1[i][j] = pt1;
-                        if (debug){System.out.printf("%.2f; ", a1[i][j]);}
+                        if (debug) {
+                            System.out.printf("%.2f; ", a1[i][j]);
+                        }
                         break;
                     default:
                         pt1 = Math.pow(((Math.tan(x[j]) - 0.25) / Math.log(Math.abs(x[j]))), 2) / 2;
                         a1[i][j] = Math.tan(Math.pow(Math.sin(Math.cbrt(x[j])), pt1));
-                        if (debug){System.out.printf("%.2f; ", a1[i][j]);}
+                        if (debug) {
+                            System.out.printf("%.2f; ", a1[i][j]);
+                        }
+
                         break;
                 }
-                if(debug){System.out.println();}
+                if (debug) {
+                    System.out.println();
+                }
             }
         }
     }
 
     public static void main(String[] args) {
-        Lab1 lab1 = new Lab1();
-        lab1.Print();
+        Lab1();
+        //lab1.Print();
+        Print();
     }
 }
